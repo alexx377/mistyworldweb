068f8bb68dffd9cfb4a806ffe1676d9c05f1d5f8
alexx377 <alexx377@gmail.com>
2022-03-12 00:18:03 +0700

    Заглушка создана

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/css', express.static(path.join(__dirname, 'styles')));
+app.use('/img', express.static(path.join(__dirname, 'imgs')));
+
 app.get('*', function(req, res) {
-  res.send('Пока здесь ничего нет, но кто знает, что будет завтра...');
+  res.render('main');
 });
 
 app.listen('8080', '127.0.0.1', function(err) {
