c1a54b9817ce46b8d9f3301f93705f443958af5e
alexx377 <alexx377@gmail.com>
2022-03-12 15:15:00 +0700

    Bot

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const bot = require('./telegram.js');
+
+app.use(express.json());
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/css', express.static(path.join(__dirname, 'styles')));
 app.use('/img', express.static(path.join(__dirname, 'imgs')));
+app.use('/fnt', express.static(path.join(__dirname, 'fonts')));
+
+app.use('/t', bot);
 
 app.get('*', function(req, res) {
   res.render('main');
---
+++ telegram.js
@@ -0,0 +1,36 @@
+const bb = require('bluebird');
+const express = require('express');
+const router = express.Router();
+
+bb.config({
+  cancellation: true
+});
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = '5031454420:AAE2NgxkFw_cDa2OTbk7SaeFxQfbF--sTng';
+const url = 'https://misty-world.ru';
+
+const bot = new TelegramBot(token);
+
+bot.setWebHook(`${url}/t`);
+
+bot.onText(/\/test (.+)/, function(msg, match) {
+
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', function(msg) {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Моя твоя не понимать.');
+});
+
+router.post('/', function(req, res) {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+module.exports = router;
